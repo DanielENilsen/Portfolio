25fa1454350703a79e79a849f7655f7b1b0eb6d5
unknown <danieleidenilsen@hotmail.com>
2020-03-19 14:14:35 +0100

    update

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-
+window.addEventListener('scroll',textApper);
 let textDisplay = document.querySelectorAll(".spanclass");
 let seeMore = document.getElementById('seemore');
-let artprojects = document.querySelectorAll('art');
+let artprojects = document.querySelector('.art');
 var sectionTwo = document.getElementById('sectionTwo');
+let smoothBehavor = document.querySelector("html");
+let myPro = document.getElementById('myPro');
 
 
+setTimeout(function() { 
+    smoothBehavor.style.scrollBehavior = "smooth";
+},1700);
 
 
 function textApper() { 
-    var textApper  = document.querySelector(".section-2");
+    var textApper  = document.querySelector(".art");
     var newpos=  textApper.getBoundingClientRect().top;
-    var textpos = window.innerHeight / 2;
-       console.log("Hello worldf")
-    if(newpos <  textpos) { 
-        textApper.classList.add('displaySectiontwo');        
-        }  
-    }
+    var textpos = window.innerHeight / 0.6;
+    
+    
+    if(newpos <  textpos) {   
+        textApper.classList.add('displaySectiontwo');  
+        myPro.style.display = "block";   
+         
+    }  
+    cardsPopup();
+
+}
 
 
 function delay(time) { 
@@ -39,4 +49,22 @@ setTimeout(function() {
 },1500)
 
 
-window.addEventListener('scroll',textApper);
+
+ async function cardsPopup() { 
+   
+    for(let i = 0; i < artprojects.length;i++) { 
+        console.log("Hello worldf")
+        await delay(300);
+       
+       if(artprojects[i].style.display == "block") { 
+           artprojects[i].style.display = "none";
+       } else { 
+        artprojects[i].style.display = "block";
+       }
+
+    }
+
+
+}
+
+
